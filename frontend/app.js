dde383dad1547a0d9de25090e64db009fe4dc7d1
Htoo Htet <htoohtethtet331@gmail.com>
2026-07-10 15:56:08 +0630

    style: specific modal preview layout for 3 images

--- frontend/app.js
+++ frontend/app.js
@@ -151,7 +151,10 @@ function setupUI() {
                     currentLayoutType = isLandscape ? 'grid-3-landscape' : 'grid-3-portrait';
                 }
                 
-                previewGrid.className = `post-images-grid ${currentLayoutType}`;
+                let previewClass = currentLayoutType;
+                if (count === 3) previewClass = 'preview-grid-3';
+                
+                previewGrid.className = `post-images-grid ${previewClass}`;
                 previewGrid.innerHTML = '';
                 
                 files.forEach((file, index) => {
